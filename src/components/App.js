123f006fdb39b94a22155f2ef95dbce2e5a5684c
Akshay <mandaakshay20@gmail.com>
2025-12-25 11:38:59 +0530

    Made changes

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,13 @@ import "../styles/App.css";
 
 const initialProducts = [
   { id: 1, name: "Samsung Galaxy S8 64GB Black", price: 16303, description: "Galaxy S8 description", image: "https://i.imgur.com/8zQZ7Zz.jpg" },
-  { id: 2, name: "Samsung Galaxy S9 64GB Black", price: 20888, description: "Galaxy S9 description", image: "https://i.imgur.com/HKJp6kE.jpg" },
-  { id: 3, name: "Samsung Galaxy S8+ 64GB Black", price: 18701, description: "Galaxy S8+ description", image: "https://i.imgur.com/a9ZgQyK.jpg" },
-  { id: 4, name: "Xiaomi Redmi Note 8 Pro", price: 12000, description: "Redmi Note 8 Pro", image: "https://i.imgur.com/1bX5QH6.jpg" },
-  { id: 5, name: "OnePlus 6T", price: 27000, description: "OnePlus 6T description", image: "https://i.imgur.com/6YJ5XK2.jpg" },
-  { id: 6, name: "Google Pixel 3", price: 45000, description: "Pixel 3 description", image: "https://i.imgur.com/ZXBtVw7.jpg" },
-  { id: 7, name: "Nokia 7 Plus", price: 15000, description: "Nokia 7 Plus description", image: "https://i.imgur.com/7vQD0fPs.jpg" },
-  { id: 8, name: "Motorola Moto G7", price: 13000, description: "Moto G7 description", image: "https://i.imgur.com/IDgG6iE.jpg" }
+  { id: 2, name: "Samsung Galaxy S9 64GB Black", price: 20888, description: "Galaxy S9 description", image: "https://i.imgur.com/8zQZ7Zz.jpg" },
+  { id: 3, name: "Samsung Galaxy S8+ 64GB Black", price: 18701, description: "Galaxy S8+ description", image: "https://i.imgur.com/8zQZ7Zz.jpg" },
+  { id: 4, name: "Xiaomi Redmi Note 8 Pro", price: 12000, description: "Redmi Note 8 Pro", image: "https://i.imgur.com/8zQZ7Zz.jpg" },
+  { id: 5, name: "OnePlus 6T", price: 27000, description: "OnePlus 6T description", image: "https://i.imgur.com/8zQZ7Zz.jpg" },
+  { id: 6, name: "Google Pixel 3", price: 45000, description: "Pixel 3 description", image: "https://i.imgur.com/8zQZ7Zz.jpg" },
+  { id: 7, name: "Nokia 7 Plus", price: 15000, description: "Nokia 7 Plus description", image: "https://i.imgur.com/8zQZ7Zz.jpg" },
+  { id: 8, name: "Motorola Moto G7", price: 13000, description: "Moto G7 description", image: "https://i.imgur.com/8zQZ7Zz.jpg" }
 ];
 
 export default function App() {
